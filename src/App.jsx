918b9ba8c66b0fc5aba91adbe955cb7e78391e8a
riddsep <ridoseptiawan00@gmail.com>
2024-09-30 16:49:19 +0700

    feat: add CRUD todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activity, setActivity] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [edit, setEdit] = useState({});
+
+  console.log(edit);
+  function generateId() {
+    return Date.now();
+  }
+
+  function saveHandlerTodo(e) {
+    e.preventDefault();
+
+    if (edit.id) {
+      const updateTodo = {
+        id: edit.id,
+        activity,
+      };
+      const matchingTodo = todos.findIndex((todo) => todo.id === edit.id);
+      const updateTodos = [...todos];
+      updateTodos[matchingTodo] = updateTodo;
+      setTodos(updateTodos);
+      return cancelHandlerTodo();
+    }
+
+    setTodos([
+      ...todos,
+      {
+        id: generateId(),
+        activity: activity,
+      },
+    ]);
+    setActivity("");
+  }
+
+  function removeHandlerTodo(todoId) {
+    const matchingTodo = todos.filter((todo) => todo.id !== todoId);
+    setTodos(matchingTodo);
+    edit.id && cancelHandlerTodo();
+  }
+  function editHandlerTodo(todo) {
+    setActivity(todo.activity);
+    setEdit(todo);
+  }
+  function cancelHandlerTodo() {
+    setEdit({});
+    setActivity("");
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Simple TodoList</h1>
+      <form onSubmit={saveHandlerTodo}>
+        <div>
+          <input
+            type="text"
+            placeholder="Masukkan aktivitas anda"
+            value={activity}
+            onChange={(e) => {
+              setActivity(e.target.value);
+            }}
+          />
+          <button type="submit">{edit.id ? "Simpan" : "Tambah"}</button>
+          {edit.id && <button onClick={cancelHandlerTodo}>Batal edit</button>}
+        </div>
+      </form>
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              {todo.activity}{" "}
+              <button
+                onClick={() => {
+                  editHandlerTodo(todo);
+                }}
+              >
+                Edit
+              </button>
+              <button
+                onClick={() => {
+                  removeHandlerTodo(todo.id);
+                }}
+              >
+                Hapus
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
